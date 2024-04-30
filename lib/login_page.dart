6fba133f75e2dced42cb2ff16eec2b546fc24f64
Abdullahi Ismail <aismail@parkway.ng>
2024-04-30 12:30:48 +0100

    Update for Biometric authentication

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -197,7 +197,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.transparent,
         body: SafeArea(
         child: Center(
         child: SingleChildScrollView(
