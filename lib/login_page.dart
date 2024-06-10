c7fb7c6deec84e4da56acaddff4c2ff6d3fd7649
Abdullahi Ismail <aismail@parkway.ng>
2024-06-10 14:08:34 +0100

    update on webview

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -18,8 +18,6 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final _firebaseMessaging = FirebaseMessaging.instance;
-  late final  email = null;
-  late final  password = null;
   bool _isPasswordVisible = false;
   bool _isLoading = false;
   bool _isLoggedIn = false;
@@ -29,8 +27,7 @@ class _LoginPageState extends State<LoginPage> {
 
   static const String _loginUrl = 'https://automationapi.lotuscapitallimited.com/api/user/mobile/login';
 
-  Future<void> handleBackgroundMessage(RemoteMessage message) async {
-  }
+  Future<void> handleBackgroundMessage(RemoteMessage message) async {}
 
   @override
   void initState() {
@@ -63,19 +60,18 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Future<bool> _canAuthenticate() async =>
-  await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
+      await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
 
   Future<bool> authenticate() async {
-
     try {
       if (!await _canAuthenticate()) return false;
 
       return await _auth.authenticate(
-          localizedReason: 'Use Face ID to authenticate',
+        localizedReason: 'Use Face ID to authenticate',
         options: const AuthenticationOptions(
           useErrorDialogs: true,
           stickyAuth: true,
-        )
+        ),
       );
     } catch (e) {
       debugPrint('Error: $e');
@@ -99,7 +95,7 @@ class _LoginPageState extends State<LoginPage> {
       String? email = prefs.getString('email');
       String? password = prefs.getString('password');
       if (email != null && password != null) {
-        _login(email, password);
+        _login(email, password, isBiometric: true);
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Login first to enable biometric authentication')),
@@ -108,34 +104,7 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  Future<Future<bool?>> _promptBiometricSetup() async {
-    // Show dialog asking the user to enable biometric authentication
-    return showDialog<bool>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Enable Biometric Authentication?'),
-        content: Text('Do you want to enable biometric authentication for future logins?'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false); // User declined
-            },
-            child: Text('No'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true); // User accepted
-            },
-            child: Text('Yes'),
-          ),
-        ],
-      ),
-    );
-  }
-
-
-
-  Future<void> _login(String email, String password) async {
+  Future<void> _login(String email, String password, {bool isBiometric = false}) async {
     setState(() {
       _isLoading = true;
     });
@@ -163,15 +132,17 @@ class _LoginPageState extends State<LoginPage> {
         }),
       );
 
-      print('Payload Sent!: $email , $password, $deviceToken');
+      print('Payload Sent!: $email, $password, $deviceToken');
 
       final Map<String, dynamic> data = jsonDecode(response.body);
       final String message = data['message'];
 
       if (response.statusCode == 200) {
         final prefs = await SharedPreferences.getInstance();
-        await prefs.setString('email', email);
-        await prefs.setString('password', password);
+        if (!isBiometric) {
+          await prefs.setString('email', email);
+          await prefs.setString('password', password);
+        }
         print('Login successful: $data');
         // Check if password is on reset mode
         final int onReset = data['onreset'];
@@ -207,191 +178,172 @@ class _LoginPageState extends State<LoginPage> {
             ),
           );
         }
-      } else if (response.statusCode == 401) {
-        // Handle 401 Unauthorized error
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message)),
-        );
-      } else if (response.statusCode == 404) {
-        // Handle 404 Not Found error
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message)),
-        );
-      } else if (response.statusCode == 500) {
-        // Handle 500 Internal Server Error
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message)),
-        );
       } else {
-        // Handle other status codes
-        print('Login failed: ${response.statusCode}');
-        print('Response body: ${response.body}');
+        // Handle errors based on status codes
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(message)),
+        );
       }
     } catch (error) {
       print('Error during login: $error');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('An error occured, please contact the system administrator')),
+        SnackBar(content: Text('An error occurred, please contact the system administrator')),
       );
-
-    }
-    finally {
+    } finally {
       setState(() {
         _isLoading = false;
       });
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SafeArea(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
         child: Container(
           color: Colors.white,
-        child: Center(
-        child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                'assets/LotusLogo.png',
-                height: 100,
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                  ),
-                ),
-              ),
-              SizedBox(height: 25.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: !_isPasswordVisible,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: BorderSide(
-                      color: Colors.redAccent, // Set your desired color here
-                      width: 2.0, // Set the width of the border
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset(
+                      'assets/LotusLogo.png',
+                      height: 100,
                     ),
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                      color: Colors.grey[800],
+                    SizedBox(height: 16.0),
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isPasswordVisible = !_isPasswordVisible;
-                      });
-                    },
-                  ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25.0),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: !_isPasswordVisible,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(
+                            color: Colors.redAccent, // Set your desired color here
+                            width: 2.0, // Set the width of the border
+                          ),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.grey[800],
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32.0),
+                    ElevatedButton(
+                      onPressed: _isLoading
+                          ? null
+                          : () {
+                        String email = _emailController.text;
+                        String password = _passwordController.text;
+                        _login(email, password);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Color(0xFF8E1611),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        minimumSize: Size(double.infinity, 50),
+                      ),
+                      child: _isLoading
+                          ? SizedBox(
+                        width: 24.0,
+                        height: 24.0,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2.0,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          backgroundColor: Color(0xFF8E1611),
+                        ),
+                      )
+                          : Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    if (Platform.isIOS)
+                      GestureDetector(
+                        onTap: _authenticateBiometric,
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/FaceID.png',
+                              width: 70,
+                              height: 70,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'Login with Face ID',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    if (Platform.isAndroid)
+                      GestureDetector(
+                        onTap: _authenticateBiometric,
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'assets/Fingerprint.png',
+                              width: 70,
+                              height: 70,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'Login with Fingerprint',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                  ],
                 ),
               ),
-              SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: _isLoading
-                    ? null
-                    :  () {
-                  String email = _emailController.text;
-                  String password = _passwordController.text;
-                  _login(email, password);
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Color(0xFF8E1611),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  minimumSize: Size(double.infinity, 50),
-                ),
-                child: _isLoading
-                    ? SizedBox(
-                  width: 24.0,
-                  height: 24.0,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 2.0,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                    backgroundColor: Color(0xFF8E1611),
-                  ),
-                )
-                    : Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 50),
-              if (Platform.isIOS)
-                GestureDetector(
-                  onTap: _authenticateBiometric,
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/FaceID.png',
-                        width: 70,
-                        height: 70,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Login with Face ID',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ),
-              if (Platform.isAndroid)
-                GestureDetector(
-                  onTap: _authenticateBiometric,
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/Fingerprint.png',
-                        width: 70,
-                        height: 70,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Login with Fingerprint',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ),
-            ],
+            ),
           ),
         ),
       ),
-        ),
-        ),
-        )
     );
   }
-  }
-
-
+}
